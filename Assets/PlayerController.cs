bc260d68843acb2e138c10a444287df88321f60b
Cay <cay.henning@proton.me>
2023-09-16 04:10:40 -0400

    Add assets

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        transform.position = position;
+    }
+}
