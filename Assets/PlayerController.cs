340d7f07829bb0af96e545ea22717a1cf4cebfa5
Cay <cay.henning@proton.me>
2023-09-16 19:31:27 -0400

    Modifies movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D rigidbody2d;
+    Rigidbody2D rb;
+    BoxCollider2D coll;
     float horizontal;
     float vertical;
     public Animator thisAnim;
     public SpriteRenderer sr;
     public bool isJumping = false;
+    public LayerMask jumpableGround;
     
-    public float jumpForce = 0.1f;
+    public float jumpForce;
+    public float moveSpeed;
+    public float waitTime;
+    float timePassed = 0.0f;
     
     
     // Start is called before the first frame update
@@ -19,7 +24,8 @@ public class PlayerController : MonoBehaviour
     {
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 10;
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -27,19 +33,8 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-    }
 
-    void FixedUpdate()
-    {
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        if (!isJumping && vertical > 0) {
-            // position.y = position.y + jumpForce * vertical;
-            rigidbody2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isJumping = true;
-        }
-
-        if (horizontal > 0 || horizontal < 0) {
+        if (horizontal > 0.1f || horizontal < -0.1f) {
             thisAnim.SetBool("isWalking", true);
             if (horizontal < 0) {
                 sr.flipX = true;
@@ -49,13 +44,31 @@ public class PlayerController : MonoBehaviour
         } else {
             thisAnim.SetBool("isWalking", false);
         }
-        transform.position = position;
+
+        timePassed += Time.deltaTime;
     }
 
-    void OnCollisionEnter2D(Collision2D collider) {
-        Debug.Log(collider.gameObject.name);
-        if (collider.gameObject.tag == "Ground") {
-            isJumping = false;
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f * horizontal;
+        transform.position = position;
+        if (IsGrounded() && vertical > 0.1f && timePassed > waitTime) {
+            // position.y = position.y + jumpForce * vertical;
+            rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            timePassed = 0f;
         }
+    }
+
+    // void OnCollisionEnter2D(Collision2D collider) {
+    //     Debug.Log(collider.gameObject.name);
+    //     if (collider.gameObject.tag == "Ground") {
+    //         isJumping = false;
+    //     }
+    // }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }
